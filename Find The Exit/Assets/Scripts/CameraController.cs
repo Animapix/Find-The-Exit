84b0dc91c2206172bfa5ce0141c5d2cc17cfd625
Nicolas POINTET <inovasong@me.com>
2022-02-25 12:07:02 +0100

    Camera movements correction

--- Find The Exit/Assets/Scripts/CameraController.cs
+++ Find The Exit/Assets/Scripts/CameraController.cs
@@ -12,9 +12,17 @@ public class CameraController : MonoBehaviour
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothFactor);
         transform.position = smoothPosition;
-        transform.LookAt(smoothPosition);
+
+
+
+        transform.eulerAngles = new Vector3(Map(smoothPosition.x, 34, 82, 64, 54), transform.eulerAngles.y, transform.eulerAngles.z);
+        offset = new Vector3(Map(smoothPosition.x, 34, 82, 50, 25), offset.y, offset.z);
+
     }
 
 
-
+    private float Map(float input, float inputMin, float inputMax, float min, float max)
+    {
+        return min + (input - inputMin) * (max - min) / (inputMax - inputMin);
+    }
 }
